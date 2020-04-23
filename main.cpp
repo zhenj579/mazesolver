18b42f45eef60f77da7429f449db19084cd53672
zhenj579 <50351080+zhenj579@users.noreply.github.com>
2020-04-22 22:35:17 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <queue>
 #include "Maze.hpp"
 
 using namespace std;
@@ -13,6 +15,7 @@ namespace solutions
             if (a_node->getDirectionNode(dir) == nullptr || a_node->getDirectionNode(dir)->isWall() || a_node->getDirectionNode(dir)->isVisited())
             {
                 wall_counter++;
+
             }
         }
         return wall_counter;
@@ -20,36 +23,75 @@ namespace solutions
 
     bool canTravel(MazeNode *a_node)
     {
-        if (a_node->isVisited() || a_node->isWall())
+        if (a_node == nullptr || a_node->isVisited() || a_node->isWall())
         {
             return false;
         }
         return true;
     }
 
-    void DFS(Maze &a_maze, std::vector<MazeNode> &path)
+    bool hasUnvisitedNeighbors(MazeNode *node)
     {
-        if(a_maze.getCurrentNode() != nullptr && a_maze.getCurrentNode() == a_maze.getLastNode())
+        for(directions::nesw dir = directions::NORTH; dir <= directions::WEST; dir = directions::nesw(dir + 1))
         {
-            path.push_back(*(a_maze.getCurrentNode())); // if path found an end add the last node and return
+            if(node != nullptr && canTravel(node->getDirectionNode(dir)))
+                return true;
+        }
+        return false;
+    }
+
+    void DFS(MazeNode *curr, MazeNode *last, std::vector<MazeNode> &path)
+    {
+
+    }
+
+    void printStack(stack<MazeNode *> &s)
+    {
+        if(s.empty())
             return;
-        }
-        a_maze.getCurrentNode()->setVisited();
-        path.push_back(*(a_maze.getCurrentNode()));
-        for(directions::nesw dir = directions::NORTH; dir <= directions::WEST; dir = directions::nesw(dir + 1)) // explore neighbors
-        {
-            if(a_maze.getCurrentNode()->getDirectionNode(dir) != nullptr && canTravel(a_maze.getCurrentNode()->getDirectionNode(dir)))
-            {
-                a_maze.setCurrentNode(a_maze.getCurrentNode()->getDirectionNode(dir));
-                DFS(a_maze, path);
-            }
-        }
+        
+        MazeNode *n = s.top();
+        s.pop();
+
+        printStack(s);
+
+        cout<<n->getStrPos()<<endl;
+
+        s.push(n);
     }
 
     std::vector<MazeNode> solveDFS(Maze &a_maze)
     {
         std::vector<MazeNode> path;
-        DFS(a_maze, path);
+        // DFS(a_maze.getFirstNode(), a_maze.getLastNode(), path);
+        std::stack<MazeNode *> s;
+        MazeNode *node = a_maze.getFirstNode();
+        s.push(node);
+        while(!s.empty())
+        {
+            node = s.top();
+            node->setVisited(); // if node isn't visited then set it to visited
+            if(node == a_maze.getLastNode())
+            {
+                break;
+            }
+
+            if(!hasUnvisitedNeighbors(node))
+            {
+                s.pop();
+                node = s.top();
+            }
+
+            for(directions::nesw dir = directions::NORTH; dir <= directions::WEST; dir = directions::nesw(dir + 1))
+            {
+                if(canTravel(node->getDirectionNode(dir)))
+                {
+                    s.push(node->getDirectionNode(dir));
+                }
+            }
+
+        }
+        printStack(s);
         return path;
     }
 
@@ -69,6 +111,7 @@ namespace solutions
 int main()
 {
     Maze m("data/maze_1.csv");
+    // solutions::solveDFS(m);
     for(auto it : solutions::solveDFS(m))
     {
         std::cout<<it.getStrPos()<<std::endl;
